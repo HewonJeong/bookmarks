b157be7529b7509cebef5bec199e8a31236ed7e6
Hewon Jeong <hewon.jng@gmail.com>
2018-10-14 23:33:29 +0900

    feat(bookmarks): add postBookmark controller

--- src/routes/bookmarks/bookmarks.ctrl.ts
+++ src/routes/bookmarks/bookmarks.ctrl.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import Bookmark from '../../database/models/Bookmark';
 
 export const checkBody = (
   req: Request,
@@ -6,7 +7,29 @@ export const checkBody = (
   next: NextFunction
 ) => {};
 
-export const postBookmark = async (req: Request, res: Response) => {};
+export const postBookmark = async (req: Request, res: Response) => {
+  type Body = {
+    title: string;
+    url: string;
+    description: string;
+    image: string;
+  };
+  const body: Body = req.body;
+  try {
+    const bookmark = await Bookmark.create({
+      ...body,
+      createdBy: req.user.id,
+    });
+    res.json(bookmark);
+  } catch (e) {
+    res.status(500);
+    res.json(
+      new Error(
+        'Internel server error during writing bookmark object to the database'
+      )
+    );
+  }
+};
 
 export const deleteBookmark = async (
   req: Request,
