1673ff9e9ee3e9f07e9cb4b6a4b83416bb73eb6e
Hewon Jeong <hewon.jng@gmail.com>
2018-10-14 20:45:37 +0900

    feat(routes): apply bookmark controllers to router

--- src/routes/bookmarks/index.ts
+++ src/routes/bookmarks/index.ts
@@ -1,5 +1,16 @@
+import {
+  postBookmark,
+  getBookmarks,
+  deleteBookmark,
+  checkBody,
+} from './bookmarks.ctrl';
 import { Router } from 'express';
+import { isAuthenticated } from '../../config/passport';
 
 const router = Router();
+
+router.post('/', isAuthenticated, checkBody, postBookmark);
+router.get('/:userId', getBookmarks);
+router.delete('/:id', isAuthenticated, deleteBookmark);
 
 export default router;
